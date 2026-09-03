8dc9202ccbcfc1f8f56e48dddccd0f5126da8aea
skirkzhdimenya-source <skirkzhdimenya@gmail.com>
2026-09-03 19:57:26 +0200

    Update home.lua

--- home.lua
+++ home.lua
@@ -7,3 +7,28 @@ G.UI = G.UI or {}
 G.Functions = G.Functions or {}
 
 print("[DeadEye] home.lua работает")
+local BASE = "https://raw.githubusercontent.com/skirkzhdimenya-source/DeadEye/main/"
+
+local function loadFile(name)
+    local success, result = pcall(function()
+
+        local code = game:HttpGet(BASE .. name)
+
+        local func, err = loadstring(code)
+
+        assert(func, err)
+
+        return func()
+
+    end)
+
+    if success then
+        print("[DeadEye] " .. name .. " успешно запущен")
+    else
+        warn("[DeadEye] ошибка " .. name .. ": " .. tostring(result))
+    end
+end
+
+loadFile("main.lua")
+loadFile("vis.lua")
+loadFile("oth.lua")
