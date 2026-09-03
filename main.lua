5215f7a317b2a8d210fc7441d34e1b2c37b39831
skirkzhdimenya-source <skirkzhdimenya@gmail.com>
2026-09-03 20:48:19 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -251,18 +251,12 @@ do
     }
 
     local PARRY_RADIUS = 7.7
-
-    print("[Parry] 1")
     
     local ParryModule =
         ReplicatedStorage.Modules.Items.ParryClient
     
-    print("[Parry] 2", ParryModule:GetFullName())
-    
     local ParryClient =
         require(ParryModule)
-    
-    print("[Parry] 3")
 
     local emoteHandler =
         ReplicatedStorage
